8de8d065c196b7def8899f3f3be953c7c10274ac
aleksander.vale <aleksander.vale@sofex.com.br>
2024-04-26 20:48:13 -0300

    reorganize files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env';
+import { patchNestJsSwagger } from 'nestjs-zod';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService<Env, true>>(ConfigService);
+  patchNestJsSwagger();
+
+  const config = new DocumentBuilder()
+    .setTitle('Area API')
+    .setDescription('Uma api para uma área de membros')
+    .setVersion('1.0')
+    .setBasePath('/api')
+    .addTag('MEMBROS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors();
+
+  const port = configService.get('PORT', { infer: true });
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
