d105dfa9142bb3f4bad707fa8dea2390e1435c0e
aleksander.vale <aleksander.vale@sofex.com.br>
2024-04-26 22:18:14 -0300

    configuacao app

---
+++ app/gatosdoparque/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export function Home() {
+  return (
+    <View>
+      <Text>Gatos do Parque</Text>
+      <Text>Seja bem-vindo ao aplicativo dos gatos do parque!</Text>
+      <Text>Aqui você pode ver fotos dos gatos do parque, saber mais sobre eles e até adotar um!</Text>
+    </View>
+  );
+}
