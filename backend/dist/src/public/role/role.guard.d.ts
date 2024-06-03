07afadd87d30fc815812abd329a8eb8ec98cc504
aleksander.vale <aleksander.vale@sofex.com.br>
2024-06-03 20:35:13 -0300

    remove dist

--- backend/dist/src/public/role/role.guard.d.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
-export declare class RoleGuard implements CanActivate {
-    private reflector;
-    constructor(reflector: Reflector);
-    matchRoles(roles: string[], userRole: string): boolean;
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean>;
-}
--- backend/dist/src/utils/format.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function formatToPhoneNumber(phone?: string): string | undefined;
-export declare function formatToDocument(document?: string): string | undefined;
